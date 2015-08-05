b4d9518f60ca21bdd55cd6a6e7833a01baa2e961
vathymut <vathymut@gmail.com>
2015-08-05 11:54:30 -0400

    Renamed function argument settlement_date to start_date.

--- R/wal-to-date.R
+++ R/wal-to-date.R
@@ -3,24 +3,24 @@
 #' Link for more details is here: 
 #' \url{https://www.cmhc-schl.gc.ca/en/hoficlincl/mobase/upload/nha_mbs_indemnity_calculation_methodology.pdf}
 #'
-#' @param settlement_date Settlement date.
+#' @param start_date Starting date.
 #' @param wal Weighted average life of NHA MBS pool.
 #'
 #' @return wal_date Weighted average life as a date.
 #'
 #' @examples
-#' settlement_date <- lubridate::ymd( "2015-01-30" )
-#' wal_to_date( settlement_date, 3.25 )
-#' wal_to_date( settlement_date, 4.89 )
+#' start_date <- lubridate::ymd( "2015-01-30" )
+#' wal_to_date( start_date, 3.25 )
+#' wal_to_date( start_date, 4.89 )
 #'
-wal_to_date <- function( settlement_date, wal ){
+wal_to_date <- function( start_date, wal ){
   # Validate input
-  stopifnot( lubridate::is.POSIXct( settlement_date ), is.numeric( wal ) )
+  stopifnot( lubridate::is.POSIXct( start_date ), is.numeric( wal ) )
   
   # Get number of days elapsed
   days_elapsed <- round( wal*365.25, 0 )
   
   # Get wal as a date
-  wal_date <- settlement_date + lubridate::days( days_elapsed )
+  wal_date <- start_date + lubridate::days( days_elapsed )
   return( wal_date )
 }
