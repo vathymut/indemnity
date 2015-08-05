17f03d65035c61578991a9fab2587ae87423d7f9
vathymut <vathymut@gmail.com>
2015-08-05 11:53:40 -0400

    Renamed function argument settlement_date to retrieval_date.

--- R/annual-to-bey.R
+++ R/annual-to-bey.R
@@ -1,23 +1,23 @@
 #' Convert the annual rate to the bond equivalent yield
 #'
-#' @param settlement_date Settlement date.
+#' @param retrieval_date Retrieval date.
 #' @param maturity_date Maturity date.
 #' @param annual_rate Annual rate.
 #'
 #' @return bey Bond equivalent yield.
 #'
 #' @examples
-#' settlement_date <- lubridate::ymd( "2015-01-30" )
+#' retrieval_date <- lubridate::ymd( "2015-01-30" )
 #' maturity_date <- lubridate::ymd( "2015-01-30" )
-#' annual_to_bey( annual_rate = 0.02, settlement_date, maturity_date )
+#' annual_to_bey( annual_rate = 0.02, retrieval_date, maturity_date )
 #'
-annual_to_bey <- function( annual_rate, settlement_date, maturity_date ){
+annual_to_bey <- function( annual_rate, retrieval_date, maturity_date ){
   # Validate input
-  stopifnot( lubridate::is.POSIXct( settlement_date ),
+  stopifnot( lubridate::is.POSIXct( retrieval_date ),
              lubridate::is.POSIXct( maturity_date ) )
   
   # Get number of days between settlement and maturity
-  dates_interval <- lubridate::interval( settlement_date, maturity_date )
+  dates_interval <- lubridate::interval( retrieval_date, maturity_date )
   t <- dates_interval / lubridate::edays( 1 )
   
   # Group common terms
