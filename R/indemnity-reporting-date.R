ed6242a8a970d47e5e30c0c3af393f6fe5dcca75
vathymut <vathymut@gmail.com>
2015-09-28 13:04:25 -0400

    Changed function args settlement_date to starting_date

--- R/indemnity-reporting-date.R
+++ R/indemnity-reporting-date.R
@@ -6,18 +6,18 @@
 #' Link for more details is here: 
 #' \url{https://www.cmhc-schl.gc.ca/en/hoficlincl/mobase/upload/nha_mbs_indemnity_calculation_methodology.pdf}
 #'
-#' @param settlement_date Settlement date.
+#' @param starting_date Starting date.
 #'
 #' @return reporting_date Reporting date.
 #'
 #' @examples
-#' settlement_date <- lubridate::ymd( "2015-02-01" )
-#' indemnity_reporting_date( settlement_date )
+#' starting_date <- lubridate::ymd( "2015-02-01" )
+#' indemnity_reporting_date( starting_date )
 #'
-indemnity_reporting_date <- function( settlement_date ){
+indemnity_reporting_date <- function( starting_date ){
   
   # Get end of month (eom) date for prior month
-  first_of_month <- settlement_date
+  first_of_month <- starting_date
   lubridate::day( first_of_month ) <- 1
   eom <- first_of_month - lubridate::days( 1 )
   
