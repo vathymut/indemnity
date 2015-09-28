76bc579b7142721de5f84262f7d837f823adabc9
vathymut <vathymut@gmail.com>
2015-09-28 13:02:46 -0400

    Changed function args names to be more consistent

--- R/bey-indemnity.R
+++ R/bey-indemnity.R
@@ -1,13 +1,13 @@
-#' Calculate the interpolated bond equivalent yield for indemnity calculation.
+#' Calculate the interpolated bond equivalent yield.
 #'
 #' Link for more details is here: 
 #' \url{https://www.cmhc-schl.gc.ca/en/hoficlincl/mobase/upload/nha_mbs_indemnity_calculation_methodology.pdf}
 #' 
-#' @param bey_short Bond equivalent yield of short-maturity tenor.
-#' @param bey_long Bond equivalent yield of long-maturity tenor.
-#' @param date_short Maturity date of short-maturity tenor.
-#' @param date_long Maturity date of long-maturity tenor.
-#' @param date_wal Weighted average life as a date.
+#' @param bey_short Bond equivalent yield of short-term tenor.
+#' @param bey_long Bond equivalent yield of long-term tenor.
+#' @param short_date Maturity date of short-term tenor.
+#' @param long_date Maturity date of long-term tenor.
+#' @param wal_date Weighted average life as a date.
 #'
 #' @return bey_interpolated Interpolated yield for indemnity calculation.
 #'
@@ -16,22 +16,22 @@
 #' @examples
 #' bey_short <- 1.363
 #' bey_long <- 1.501
-#' date_short <- ymd( "2016-06-01" ) 
-#' date_long <- ymd( "2017-09-01" ) 
-#' date_wal <- ymd( "2016-11-23" ) 
-#' bey_indemnity( bey_short, bey_long, date_short, date_long, date_wal )
+#' short_date <- ymd( "2016-06-01" ) 
+#' long_date <- ymd( "2017-09-01" ) 
+#' wal_date <- ymd( "2016-11-23" ) 
+#' bey_indemnity( bey_short, bey_long, short_date, long_date, wal_date )
 #' 
-bey_indemnity <- function( bey_short, bey_long, date_short, date_long, date_wal ){
+bey_indemnity <- function( bey_short, bey_long, short_date, long_date, wal_date ){
   
   # Difference in yields
   diff_bey <- unname( bey_long - bey_short )
   
   # Difference in days between dates of wal and short(er) maturity
-  diff_num <- lubridate::interval( date_short, date_wal ) / lubridate::edays( 1 )
-  stopifnot( date_wal >= date_short ) # Sanity check
+  diff_num <- lubridate::interval( short_date, wal_date ) / lubridate::edays( 1 )
+  stopifnot( wal_date >= short_date ) # Sanity check
   
   # Difference in days between dates of long(er) and short(er) maturity
-  diff_denom <- lubridate::interval( date_short, date_long ) / lubridate::edays( 1 )
+  diff_denom <- lubridate::interval( short_date, long_date ) / lubridate::edays( 1 )
   
   # Calculate the interpolated GoC yield
   factor <- diff_num/diff_denom
