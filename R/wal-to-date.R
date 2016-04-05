a2560705b3198bd8fd920a2ab662dcc604d2c687
vathymut <vathymut@gmail.com>
2016-04-05 16:15:53 -0400

    Renamed start date as retrieval date.

--- R/wal-to-date.R
+++ R/wal-to-date.R
@@ -3,24 +3,24 @@
 #' Link for more details is here: 
 #' \url{https://www.cmhc-schl.gc.ca/en/hoficlincl/mobase/upload/nha_mbs_indemnity_calculation_methodology.pdf}
 #'
-#' @param start_date Starting date.
+#' @param retrieval_date Retrieval date of the yield curve.
 #' @param wal Weighted average life of NHAMBS pool.
 #'
 #' @return wal_date Weighted average life as a date.
 #'
 #' @examples
-#' start_date <- lubridate::ymd( "2015-01-30" )
-#' wal_to_date( start_date, 3.25 )
-#' wal_to_date( start_date, 4.89 )
+#' retrieval_date <- lubridate::ymd( "2015-01-30" )
+#' wal_to_date( retrieval_date, 3.25 )
+#' wal_to_date( retrieval_date, 4.89 )
 #'
-wal_to_date <- function( start_date, wal ){
+wal_to_date <- function( retrieval_date, wal ){
   # Validate input
-  stopifnot( lubridate::is.POSIXct( start_date ), is.numeric( wal ) )
+  stopifnot( lubridate::is.POSIXct( retrieval_date ), is.numeric( wal ) )
   
   # Get number of days elapsed
   days_elapsed <- round( wal*365.25, 0 )
   
   # Get wal as a date
-  wal_date <- start_date + lubridate::days( days_elapsed )
+  wal_date <- retrieval_date + lubridate::days( days_elapsed )
   return( wal_date )
 }
